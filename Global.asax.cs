31ea298e118ed0add16ee769471e0f5d1b8ddd4a
mahendragurav <mahendra.gurav@flextronics.com>
2014-10-27 12:52:21 +0530

    Added login module

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Flextronics.QMSCC.Commons.SystemIntegrations.FlexWare.AuthenticationServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace ATOMv0
 {
@@ -16,6 +18,51 @@ namespace ATOMv0
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+          if (System.Web.HttpContext.Current.Session != null)
+          {
+            if (FormsAuthentication.CookiesSupported == true)
+            {
+              if (Request.Cookies[FormsAuthentication.FormsCookieName] != null)
+              {
+                try
+                {
+                  string userName = FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value).Name;
+                  string roles = "";
+
+                  if (Session["userRoles"] != null)
+                  {
+                    userrolesws userRoles = (userrolesws)Session["userRoles"];
+                    if (userRoles != null && userRoles.roles.Count() > 0)
+                    {
+                      foreach (var item in userRoles.roles)
+                      {
+                        if (roles == "")
+                        {
+                          roles = item.name;
+                        }
+                        else
+                        {
+                          roles = roles + ";" + item.name;
+                        }
+                      }
+                    }
+                  }
+                  if (Session["userRoles"] != null)
+                    Session["userRoles"] = null;
+                  Context.User = new System.Security.Principal.GenericPrincipal(
+                       new System.Security.Principal.GenericIdentity(userName, "Forms"), roles.Split(';'));
+
+                }
+                catch (Exception ex)
+                {
+                  throw ex;
+                }
+              }
+            }
+          }
         }
     }
 }
